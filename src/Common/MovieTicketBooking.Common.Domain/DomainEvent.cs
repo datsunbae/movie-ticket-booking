f10a1336489ca40f1f8e35d84a501be2492507c9
Benjamin <manpham.dev@gmail.com>
2025-01-01 18:14:59 +0700

    set up domain

---
+++ src/Common/MovieTicketBooking.Common.Domain/DomainEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieTicketBooking.Common.Domain;
+public abstract class DomainEvent : IDomainEvent
+{
+    public Ulid Id { get; init; }
+    public DateTime OccurredAtUtc { get; init; }
+
+    protected DomainEvent()
+    {
+        Id = Ulid.NewUlid();
+        OccurredAtUtc = DateTime.UtcNow;
+    }
+
+    protected DomainEvent(Ulid id, DateTime occurredAtUtc)
+    {
+        Id = id;
+        OccurredAtUtc = occurredAtUtc;
+    }
+}
---
+++ src/Common/MovieTicketBooking.Common.Domain/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace MovieTicketBooking.Common.Domain;
+public interface IDomainEvent : INotification
+{
+    Ulid Id { get; }
+    DateTime OccurredAtUtc { get; }
+}
